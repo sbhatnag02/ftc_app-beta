71524f19e6ecc8a8c5e38a3aae407666100637e6
yankees1fan2002 <yankees1fan2002@gmail.com>
2016-09-17 22:50:18 -0400

    Added Outreach TeleOp for Merlin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MerlinOutreachTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MerlinOutreachTeleOp.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Sarthak on 9/17/2016.
  */
+@TeleOp(name="Outreach Teleop", group = "Merlin")
+
 public class MerlinOutreachTeleOp extends OpMode {
     DcMotor leftFront, leftBack, rightFront, rightBack;
 
@@ -22,30 +26,18 @@ public class MerlinOutreachTeleOp extends OpMode {
         leftBack = this.hardwareMap.dcMotor.get("left_back");
         rightBack = this.hardwareMap.dcMotor.get("right_back");
         rightFront = this.hardwareMap.dcMotor.get("right_front");
+
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        double leftPower = gamepad1.left_stick_y;
-        double rightPower = gamepad1.right_stick_y;
 
-        setPowerAll(leftPower, rightPower);
-
-        if (gamepad1.dpad_up){
-            setPowerAll(.35, .35);
-        }
-        else if (gamepad1.dpad_down){
-            setPowerAll(-.35, -.35);
-        }
-        else if (gamepad1.dpad_left){
-            setPowerAll(-.35, .35);
-        }
-        else if (gamepad1.dpad_right){
-            setPowerAll(-.35, .35);
-        }
-        else{
-            setPowerAll(0, 0);
-        }
+        leftFront.setPower(-gamepad1.left_stick_y);
+        leftBack.setPower(-gamepad1.left_stick_y);
+        rightFront.setPower(-gamepad1.right_stick_y);
+        rightBack.setPower(-gamepad1.right_stick_y);
 
     }
 }
